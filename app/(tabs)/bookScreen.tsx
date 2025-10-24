d59a99df03a84ba312bdf0d6b5b54ed69e85090b
Maria Eduarda <eduardabritto2014@gmail.com>
2025-10-23 21:02:02 -0300

    Reduce padding in bookScreen component

--- app/(tabs)/bookScreen.tsx
+++ app/(tabs)/bookScreen.tsx
@@ -98,7 +98,7 @@ const styles = StyleSheet.create({
         textAlign: "center",
         borderRadius: 20,
         marginBottom: 10,
-        padding: 10,
+        padding: 5,
         paddingTop: 5,
     },
 });
